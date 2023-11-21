128e677de885ef8242b41c59eca7a0db0f41e74c
Qroin <62214298+Qroin@users.noreply.github.com>
2023-11-21 17:21:35 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+function startAnimation() {
+    animateButton();
+    setTimeout(pickWinner, 1000);
+}
+
+function animateButton() {
+    var button = document.querySelector("button");
+    anime({
+        targets: button,
+        scale: 1.1,
+        duration: 500,
+        easing: 'easeInOutQuad',
+        direction: 'alternate',
+        loop: 2
+    });
+}
+
 function pickWinner() {
-    // 랜덤으로 제비를 뽑는 로직
     var participants = ["참가자1", "참가자2", "참가자3", "참가자4", "참가자5"];
     var winner = participants[Math.floor(Math.random() * participants.length)];
 
-    // 결과를 표시
     var resultElement = document.getElementById("result");
     resultElement.textContent = "축하합니다! " + winner + " 님은 당첨되셨습니다!";
 }
